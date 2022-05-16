9cc3e1f79dc0b9a327f9b8e4b89eb16989637ce9
Dian Rahmaji <dianrahmaji@gmail.com>
2022-05-16 14:35:47 +0700

    KMNA-8 Integrated Login Page (#11)
    * added userConstants
    
    * removed unsed constants
    
    * added userReducers
    
    * added userActions
    
    * implemented admin login

---
+++ src/store/actions/userActions.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+
+import {
+  USER_LOGIN_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGOUT
+} from '../constants/userConstants'
+
+export const login = (email, password) => async dispatch => {
+  try {
+    dispatch({ type: USER_LOGIN_REQUEST })
+
+    const { data } = await axios.post('/api/admin/login', { email, password })
+
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data })
+    localStorage.setItem('user', JSON.stringify(data))
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+    })
+  }
+}
+
+export const logout = () => dispatch => {
+  localStorage.removeItem('user')
+  dispatch({ type: USER_LOGOUT })
+}
