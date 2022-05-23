0d68758138c9be356475e27de6ca02f791ad662e
dianrahmaji <dianrahmaji@gmail.com>
2022-05-23 10:49:38 +0700

    added researcherList action

---
+++ src/store/actions/researcherActions.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import MySwal from '../../utils/sweetalert.js'
+
+import {
+  RESEARCHER_LIST_FAIL,
+  RESEARCHER_LIST_REQUEST,
+  RESEARCHER_LIST_SUCCESS
+} from '../constants/researcherConstants'
+
+export const reseaercherList = () => async dispatch => {
+  try {
+    dispatch({ type: RESEARCHER_LIST_REQUEST })
+
+    const { data } = await axios.post('/api/users')
+
+    dispatch({ type: RESEARCHER_LIST_SUCCESS, payload: data })
+  } catch (error) {
+    dispatch({
+      type: RESEARCHER_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+    })
+    MySwal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: error.response.data.messagesss
+    })
+  }
+}
