5d2f447d3d42bd5a9ae9ff145b0922cbd3a146d8
dianrahmaji <dianrahmaji@gmail.com>
2022-05-27 22:00:36 +0700

    implemented TailwindUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 const App = () => {
-  return <div></div>
+  return (
+    <div className="flex align-center justify-center h-screen">
+      <button className="inline-flex items-center px-6 py-3 my-auto border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+        Click!
+      </button>
+    </div>
+  )
 }
 
 export default App
