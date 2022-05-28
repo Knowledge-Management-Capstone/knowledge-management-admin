ec4b102daa036cf2242d46b80e00390e3b27fa0a
dianrahmaji <dianrahmaji@gmail.com>
2022-05-28 08:33:07 +0700

    registered color theme to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ['index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#073C64',
+        secondary: '#FDCB2C',
+        accent: '#1A2C43'
+      }
+    }
   },
   plugins: []
 }
