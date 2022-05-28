2bb3745ab77adcbb808844d1d4c27630cf0f3d88
dianrahmaji <dianrahmaji@gmail.com>
2022-05-28 21:24:12 +0700

    added Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,12 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+import { Routes, Route } from 'react-router-dom'
 
-import { email, password } from './utils/validation'
-
-import BaseButton from './components/generic/button/BaseButton'
-import BaseInput from './components/generic/form/BaseInput'
-import FormModal from './components/FormModal'
+import Login from './pages/Login'
 
 const App = () => {
-  const [open, setOpen] = useState(false)
-
-  const initialValues = {
-    email: '',
-    password: ''
-  }
-
-  const handleSubmit = (values, { setSubmitting }) => {
-    console.log(values)
-    setSubmitting(false)
-    setOpen(false)
-  }
-
-  useEffect(() => {
-    axios.get('/').then(res => console.log(res.data))
-  }, [])
-
   return (
-    <div className="flex align-center justify-center h-screen">
-      <BaseButton onClick={() => setOpen(true)} className="px-6 py-3 my-auto">
-        Click!
-      </BaseButton>
-      <FormModal
-        title="Payment successful"
-        open={open}
-        setOpen={setOpen}
-        initialValues={initialValues}
-        validation={{ email, password }}
-        handleSubmit={handleSubmit}
-      >
-        <BaseInput label="Email" name="email" type="email" />
-        <BaseInput label="Password" name="password" type="password" />
-      </FormModal>
-    </div>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+    </Routes>
   )
 }
 
