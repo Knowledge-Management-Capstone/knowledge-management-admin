7b509ebf5e60505068abd370d4bfdc10a5699e11
dianrahmaji <dianrahmaji@gmail.com>
2022-05-28 11:50:25 +0700

    extracted BaseModal and FormModal

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,15 @@ import { email, password } from './utils/validation'
 
 import BaseButton from './components/generic/button/BaseButton'
 import BaseInput from './components/generic/form/BaseInput'
-import BaseModal from './components/generic/modal/BaseModal'
+import FormModal from './components/FormModal'
 
 const App = () => {
   const [open, setOpen] = useState(false)
+
+  const initialValues = {
+    email: '',
+    password: ''
+  }
 
   const handleSubmit = (values, { setSubmitting }) => {
     console.log(values)
@@ -22,37 +27,17 @@ const App = () => {
       <BaseButton onClick={() => setOpen(true)} className="px-6 py-3 my-auto">
         Click!
       </BaseButton>
-      <BaseModal title="Payment successful" open={open} setOpen={setOpen}>
-        <Formik
-          initialValues={{
-            email: '',
-            password: ''
-          }}
-          validationSchema={Yup.object({ email, password })}
-          onSubmit={handleSubmit}
-        >
-          <Form>
-            <BaseInput label="Email" name="email" type="email" />
-            <BaseInput label="Password" name="password" type="password" />
-            <div className="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
-              <BaseButton
-                type="submit"
-                className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 focus:outline-none sm:col-start-2 sm:text-sm"
-              >
-                Simpan
-              </BaseButton>
-              <BaseButton
-                type="button"
-                className="mt-3 w-full inline-flex justify-center rounded-md border shadow-sm px-4 py-2 sm:mt-0 sm:col-start-1 sm:text-sm"
-                secondary
-                onClick={() => setOpen(false)}
-              >
-                Batal
-              </BaseButton>
-            </div>
-          </Form>
-        </Formik>
-      </BaseModal>
+      <FormModal
+        title="Payment successful"
+        open={open}
+        setOpen={setOpen}
+        initialValues={initialValues}
+        validation={{ email, password }}
+        handleSubmit={handleSubmit}
+      >
+        <BaseInput label="Email" name="email" type="email" />
+        <BaseInput label="Password" name="password" type="password" />
+      </FormModal>
     </div>
   )
 }
