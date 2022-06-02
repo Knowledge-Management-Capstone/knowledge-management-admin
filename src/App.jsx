070373494b6185eb7ae10d623bdd48414d7894b0
dianrahmaji <dianrahmaji@gmail.com>
2022-06-02 10:33:22 +0700

    added ResearchDetail page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import dashboard from '~/config/dashboard'
 
 import Login from '~/pages/Login'
+import ResearcherDetails from '~/pages/ReserachDetails'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       {dashboard.map(({ route, navigation }) => (
         <Route {...route} key={navigation.name} />
       ))}
+      <Route path="/research/:id" element={<ResearcherDetails />} />
       <Route path="/login" element={<Login />} />
     </Routes>
   )
