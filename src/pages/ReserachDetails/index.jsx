140d48588b0fadd75377ba6c8e2c4c506fc33162
dianrahmaji <dianrahmaji@gmail.com>
2022-06-02 10:46:40 +0700

    added BaseBreadcrumbs

--- src/pages/ReserachDetails/index.jsx
+++ src/pages/ReserachDetails/index.jsx
@@ -1,13 +1,17 @@
+import BaseBreadcrumbs from '~/components/generic/breadcrumbs/BaseBreadcrumbs'
 import DashboardLayout from '~/layouts/DashboardLayout'
+
+const pages = [
+  { name: 'Penelitian', redirect: '/research', current: false },
+  { name: 'Capstone Project', redirect: '#', current: true }
+]
 
 const ResearcherDetails = () => {
   return (
     <DashboardLayout>
       <div className="py-6">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-          <h1 className="text-2xl font-semibold text-gray-900">
-            Reserach Detail
-          </h1>
+          <BaseBreadcrumbs pages={pages} />
         </div>
         <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
           {/* Replace with your content */}
