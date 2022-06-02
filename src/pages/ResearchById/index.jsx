9dc8e25ccf47c251c49e507487b845abb8ad2d33
dianrahmaji <dianrahmaji@gmail.com>
2022-06-02 11:18:31 +0700

    added ReserachDetails

--- src/pages/ResearchById/index.jsx
+++ src/pages/ResearchById/index.jsx
@@ -1,6 +1,7 @@
 import BaseBreadcrumbs from '~/components/generic/breadcrumbs/BaseBreadcrumbs'
 import DashboardLayout from '~/layouts/DashboardLayout'
 import MemberTable from './components/MemberTable'
+import ResearchDetails from './components/ResearchDetails'
 
 const pages = [
   { name: 'Penelitian', redirect: '/research', current: false },
@@ -15,6 +16,7 @@ const ResearcherDetails = () => {
           <BaseBreadcrumbs pages={pages} />
         </div>
         <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+          <ResearchDetails />
           <MemberTable />
         </div>
       </div>
