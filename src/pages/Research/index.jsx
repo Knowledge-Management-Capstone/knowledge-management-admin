44f182b9bf9ffb38d1f1e4695a2d8b391c887a48
dianrahmaji <dianrahmaji@gmail.com>
2022-06-02 09:56:57 +0700

    added ResearchTable

--- src/pages/Research/index.jsx
+++ src/pages/Research/index.jsx
@@ -1,4 +1,5 @@
 import DashboardLayout from '~/layouts/DashboardLayout'
+import ResearchTable from './components/ResearchTable'
 
 const Research = () => {
   return (
@@ -7,7 +8,9 @@ const Research = () => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
           <h1 className="text-2xl font-semibold text-gray-900">Penelitian</h1>
         </div>
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8"></div>
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+          <ResearchTable />
+        </div>
       </div>
     </DashboardLayout>
   )
