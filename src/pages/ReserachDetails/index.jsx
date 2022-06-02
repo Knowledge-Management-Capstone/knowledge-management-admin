d415c9f2534332bdb58798fb3aaf2eecab161da4
dianrahmaji <dianrahmaji@gmail.com>
2022-06-02 11:01:47 +0700

    added MemberTable

--- src/pages/ReserachDetails/index.jsx
+++ src/pages/ReserachDetails/index.jsx
@@ -1,5 +1,6 @@
 import BaseBreadcrumbs from '~/components/generic/breadcrumbs/BaseBreadcrumbs'
 import DashboardLayout from '~/layouts/DashboardLayout'
+import MemberTable from './components/MemberTable'
 
 const pages = [
   { name: 'Penelitian', redirect: '/research', current: false },
@@ -14,11 +15,7 @@ const ResearcherDetails = () => {
           <BaseBreadcrumbs pages={pages} />
         </div>
         <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-          {/* Replace with your content */}
-          <div className="py-4">
-            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
-          </div>
-          {/* /End replace */}
+          <MemberTable />
         </div>
       </div>
     </DashboardLayout>
