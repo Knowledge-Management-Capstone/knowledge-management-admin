94c4bfc9b61bf0c959baebf4fe1eebeb18dd94ee
dianrahmaji <dianrahmaji@gmail.com>
2022-08-04 10:27:38 +0700

    chore: added new redux constants

--- src/store/constants/userConstants.js
+++ src/store/constants/userConstants.js
@@ -2,4 +2,7 @@ export const USER_LOGIN_REQUEST = 'USER_LOGIN_REQUEST'
 export const USER_LOGIN_SUCCESS = 'USER_LOGIN_SUCCESS'
 export const USER_LOGIN_FAIL = 'USER_LOGIN_FAIL'
 
+export const USER_LOADING = 'USER_LOADING'
+export const USER_LOGIN = 'USER_LOGIN'
 export const USER_LOGOUT = 'USER_LOGOUT'
+export const USER_ERROR = 'USER_ERROR'
